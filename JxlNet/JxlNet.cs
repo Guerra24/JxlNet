a338d1cda246385bf743c66a0f6614c3d3ceb29c
Lili González <lili@guerra24.net>
2026-05-09 20:06:12 -0600

    Allow loading from runtimes directory

--- JxlNet/JxlNet.cs
+++ JxlNet/JxlNet.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 
 namespace JxlNet;
 
-public static unsafe partial class JxlThreads
+public static partial class Jxl
+{
+    static Jxl()
+    {
+#if NETSTANDARD || WINDOWS10_0_17763_0_OR_GREATER
+#else
+        NativeLibrary.Load("jxl", Assembly.GetExecutingAssembly(), null);
+#endif
+    }
+}
+
+public static partial class JxlThreads
 {
     public static readonly IntPtr JxlResizableParallelRunner;
     public static readonly IntPtr JxlThreadParallelRunner;
 
-    static unsafe JxlThreads()
+    static JxlThreads()
     {
 #if NETSTANDARD || WINDOWS10_0_17763_0_OR_GREATER
         FreeLibrarySafeHandle jxl_threads;
@@ -37,7 +49,7 @@ public static unsafe partial class JxlThreads
         JxlResizableParallelRunner = PInvoke.GetProcAddress(jxl_threads, "JxlResizableParallelRunner").Value;
         JxlThreadParallelRunner = PInvoke.GetProcAddress(jxl_threads, "JxlThreadParallelRunner").Value;
 #else
-        var jxl_threads = NativeLibrary.Load("jxl_threads");
+        var jxl_threads = NativeLibrary.Load("jxl_threads", Assembly.GetExecutingAssembly(), null);
         JxlResizableParallelRunner = NativeLibrary.GetExport(jxl_threads, "JxlResizableParallelRunner");
         JxlThreadParallelRunner = NativeLibrary.GetExport(jxl_threads, "JxlThreadParallelRunner");
 #endif
